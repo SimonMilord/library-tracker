4fb3ec5d82fa0a1b25c8c4ba308ca7ee1151e6bb
Simon Milord <simon.milord@gmail.com>
2022-02-21 18:13:16 -0500

    fixed issue with array appending again and added delete button and toggle button functionalities

--- scripts/app.js
+++ scripts/app.js
@@ -1,31 +1,30 @@
-
 let myLibrary = [
-  // {
-  //   title: 'The Lord of The Rings',
-  //   author: 'JRR Tolkien',
-  //   pages: 1000,
-  //   read: true,
-  //   bookId: 0,
-  // },
-  // {
-  //   title: 'One Up Wallstreet',
-  //   author: 'Peter Lynch',
-  //   pages: 300,
-  //   read: true,
-  //   bookId: 1,
-  // },
-  // {
-  //   title: 'Sapiens',
-  //   author: 'Noah Yuval Harrari',
-  //   pages: 400,
-  //   read: false,
-  //   bookId: 2,
-  // }
+  {
+    title: 'The Lord of The Rings',
+    author: 'JRR Tolkien',
+    pages: 1000,
+    read: true,
+    bookId: 0,
+  },
+  {
+    title: 'One Up Wallstreet',
+    author: 'Peter Lynch',
+    pages: 300,
+    read: true,
+    bookId: 1,
+  },
+  {
+    title: 'Sapiens',
+    author: 'Noah Yuval Harrari',
+    pages: 400,
+    read: false,
+    bookId: 2,
+  }
 ];
+
 // gather dom elements
 const newBookBtn = document.getElementById("newbook");
 const myModal = document.getElementById('modal');
-// modal inputs:
 const titleInput = document.querySelector(".modal-input-title");
 const authorInput = document.querySelector(".modal-input-author");
 const pagesInput = document.querySelector(".modal-input-pages");
@@ -36,7 +35,7 @@ const form = document.querySelector('.book-form');
 const list = document.querySelector(".list");
 const del = document.getElementById('delete');
 
-
+// CONSTRUCTOR FUNCTION
 function Book(title, author, pages, read) {
   this.title = title
   this.author = author
@@ -77,8 +76,6 @@ function closeModal() {
 }
 
 function renderLibrary() {
-  // myLibrary.innerText = '';
-
   myLibrary.forEach((book) => {
     const bookLi = createCard('li', 'list__item');
     // top of card
@@ -86,11 +83,16 @@ function renderLibrary() {
     const cardTitle = createCard('p', 'book__info', book.title);
     const cardAuthor = createCard('p', 'book__info', 'By: ' + book.author);
     const cardPages = createCard('p', 'book__info', book.pages + ' Pages');
+    const cardStatus = createCard('p', 'book__info', showStatus());
     // bottom of card
     const cardBottom = createCard('div', 'list__card-bottom');
-    const readBtn = createCard('a', 'btn', 'Read?');
-    readBtn.setAttribute('type', 'submit');
-    readBtn.setAttribute('id', 'read');
+    // toggle switch
+    const toggle = createCard('label', 'switch');
+    const switchInput = createCard('input', 'switch__checkbox');
+    switchInput.setAttribute('type', 'checkbox');
+    const switchSpan = createCard('span', 'slider');
+    switchSpan.classList.add('round');
+
     const deleteBtn = createCard('a', 'btn', 'Delete');
     deleteBtn.setAttribute('type', 'submit');
     deleteBtn.setAttribute('id', 'delete');
@@ -99,7 +101,12 @@ function renderLibrary() {
     cardTop.appendChild(cardTitle);
     cardTop.appendChild(cardAuthor);
     cardTop.appendChild(cardPages);
-    cardBottom.appendChild(readBtn);
+    cardTop.appendChild(cardStatus);
+
+    toggle.appendChild(switchInput);
+    toggle.appendChild(switchSpan);
+    cardBottom.appendChild(toggle);
+
     cardBottom.appendChild(deleteBtn);
     bookLi.appendChild(cardTop);
     bookLi.appendChild(cardBottom);
@@ -112,12 +119,22 @@ function renderLibrary() {
 
       wholeCard.parentElement.removeChild(wholeCard);
     })
+
+    function showStatus() {
+      if (book.read === true) {
+        return "\u2705 " + "Read";
+      }
+      return "\u274C " + "Not Read";
+    }
+
+    switchInput.addEventListener('change', event => {
+      if (switchInput.checked) {
+        cardStatus.innerText = "\u2705 " + "Read";
+      } else {
+        cardStatus.innerText = "\u274C " + "Not Read";
+      }
+    })
   })
 }
 
 renderLibrary(myLibrary);
-
-
-
-
-
